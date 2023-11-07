41ff49f6563522e8935a686cd78ab28723a6e1fb
David Kebo <davidkebo@gmail.com>
2023-11-07 09:58:57 -0600

    Initial commit

---
+++ packet.py
@@ -0,0 +1,22 @@
+from scapy.all import Ether, IP, TCP, sendp
+
+# Define the Ethernet frame
+
+
+# Define the IP packet
+
+
+# Define the TCP segment
+
+
+# Define the payload
+
+
+# Combine everything into a single packet
+
+
+# Print the packet summary
+print(packet.show())
+
+# Send the packet on the network
+
